8043cd0dea95668f722333d41aaad0405dd8bb7d
Lex Crawford <lexcraw4d@gmail.com>
2022-08-08 22:18:11 -0400

    added CR stories and see all public stories

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const passport = require('passport');
 const session = require('express-session');
 const { Cookie } = require('express-session');
+const MongoStore = require('connect-mongo');
 //Load config file
 dotenv.config({ path: './config/config.env'});
 
@@ -17,12 +18,25 @@ require('./config/passport')(passport);
 connectDB();
 
 const app = express();
+
+//body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //Logging middleware
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+//Handlebars helpers
+const {formatDate, stripTags, truncate} = require('./helpers/hbs');
 //Handlebars Middleware
 app.engine('.hbs', exphbs.engine({
+    helpers: {
+        formatDate,
+        stripTags,
+        truncate
+    },
     defaultLayout: 'main',
     extname: '.hbs' 
 })
@@ -35,7 +49,10 @@ app.use(
     secret: 'acorn developer',
     resave: false,
     saveUninitialized: false,
-})
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI
+    })
+    })
 );
 
 //Passport middleware
@@ -46,6 +63,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000;
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const  Story = require('../models/Story');
 const { ensureAuth, ensureGuest} = require('../middleware/auth');
+
 //@desc Login/landing page
 //@route GET /
 router.get('/', ensureGuest, (req, res) => {
@@ -9,9 +11,20 @@ router.get('/', ensureGuest, (req, res) => {
     });
 })
 
-router.get('/dashboard', ensureAuth, (req, res) => {
-    console.log(req.user)
-    res.render('dashboard');
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    // console.log(req.user)
+    try{
+        const stories = await Story.find({user: req.user.id}).lean();
+
+        res.render('dashboard',{
+            name: req.user.firstName,
+            stories
+        });
+    }
+    catch(err){
+        console.error(err.message);
+        res.render('error/500');
+    }
 })
 
 module.exports = router;
